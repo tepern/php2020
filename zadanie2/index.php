a8364ea512ffcff0dba2673d1cd1efda5e2836eb
nata <teper.n@gmail.com>
2020-03-23 17:35:55 +0400

    Добавлены функции.

--- zadanie2/index.php
+++ zadanie2/index.php
@@ -1,5 +1,5 @@
 <form method="post" action="form.php" enctype="multipart/form-data">
-    <textarea name="text">Вставьте текст</textarea><br>
+    <textarea name="text" placeholder="Вставьте текст"></textarea><br>
     <input type="file" name="docs[]" multiple><br>
-    <input type="submit">
+    <input type="submit" value="Отправить">
 </form>     
